7475df93e9da0859ffb221410620439b975334da
zhanpengqiu <1219300839@qq.com>
2025-01-11 14:28:50 +0800

    new commit

---
+++ abi/build.rs
@@ -0,0 +1,8 @@
+fn main(){
+    tonic_build::configure()
+    .out_dir("src/pb")
+    .compile(&["protos/reservation.proto"], &["protos"])
+    .unwrap();
+
+    println!("cargo:rerun-if-changed=protos/resvervation.proto");
+}
--- abi/src/lib.rs
+++ abi/src/lib.rs
@@ -1,3 +1,6 @@
+#[allow(clippy::all)]
+mod pb;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
